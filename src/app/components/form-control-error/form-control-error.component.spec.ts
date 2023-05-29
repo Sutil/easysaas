2b9ad84b1bf83d4e339a0d0499d1afaf5959dce4
Eduardo Sutil <sutil.edu@gmail.com>
2023-05-29 16:50:11 -0300

    Test fix

--- src/app/components/form-control-error/form-control-error.component.spec.ts
+++ src/app/components/form-control-error/form-control-error.component.spec.ts
@@ -1,14 +1,16 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { ESTranslateModule } from 'src/app/services/translate/translate.module';
 import { FormControlErrorComponent } from './form-control-error.component';
 
 describe('FormControlErrorComponent', () => {
   let component: FormControlErrorComponent;
   let fixture: ComponentFixture<FormControlErrorComponent>;
 
-  beforeEach(() => {
+  beforeAll(() => {
     TestBed.configureTestingModule({
-      declarations: [FormControlErrorComponent]
+      imports: [ESTranslateModule],
+      declarations: [FormControlErrorComponent],
     });
     fixture = TestBed.createComponent(FormControlErrorComponent);
     component = fixture.componentInstance;
