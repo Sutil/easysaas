2bbe96a839571a14a952b71547d5d89f330d7801
Eduardo Sutil <sutil.edu@gmail.com>
2023-05-29 17:31:28 -0300

    test fix

--- src/app/components/password-input/password-input.component.spec.ts
+++ src/app/components/password-input/password-input.component.spec.ts
@@ -1,14 +1,18 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { FormsModule, NgControl } from '@angular/forms';
+import { FormControlErrorModule } from '../form-control-error/form-control-error.module';
 import { PasswordInputComponent } from './password-input.component';
 
 describe('PasswordInputComponent', () => {
   let component: PasswordInputComponent;
   let fixture: ComponentFixture<PasswordInputComponent>;
 
-  beforeEach(() => {
+  beforeAll(() => {
     TestBed.configureTestingModule({
-      declarations: [PasswordInputComponent]
+      imports: [FormControlErrorModule, FormsModule],
+      declarations: [PasswordInputComponent],
+      providers: [NgControl]
     });
     fixture = TestBed.createComponent(PasswordInputComponent);
     component = fixture.componentInstance;
